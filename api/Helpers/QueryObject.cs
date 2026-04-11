0175b4ee10fdac0df9f986af58e8c24e312a5993
MCVenor <michal.j.cu@gmail.com>
2026-04-11 16:00:04 +0200

    Added Sortby Url Feature

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -1,8 +1,12 @@
-﻿namespace api.Helpers
+﻿using System.Runtime.CompilerServices;
+
+namespace api.Helpers
 {
     public class QueryObject
     {
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
+        public string SortBy { get; set; } = string.Empty;
+        public bool IsDecsending { get; set; } = false;
     }
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -49,6 +49,13 @@ namespace api.Repository
             {
                 stock = stock.Where(s => s.Symbol.Contains(query.Symbol));
             }
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stock = query.IsDecsending ? stock.OrderByDescending(s => s.Symbol) : stock.OrderBy(s => s.Symbol);
+                }
+            }
 
             return await stock.ToListAsync();
         }
